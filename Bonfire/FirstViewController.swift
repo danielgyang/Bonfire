cf21e9e1211fd1c747eefc39cf7444b77e0dbff5
Daniel Yang <yangafc@bc.edu>
2020-07-24 14:43:25 -0400

    Initial Commit

---
+++ Bonfire/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Bonfire
+//
+//  Created by Daniel Yang on 7/24/20.
+//  Copyright © 2020 Daniel Yang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bonfire/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Bonfire
+//
+//  Created by Daniel Yang on 7/24/20.
+//  Copyright © 2020 Daniel Yang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
