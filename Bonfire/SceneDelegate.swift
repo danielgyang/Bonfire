9e5fb8ae001ae93cea965f8115e48c1dc8b9f69e
Daniel Yang <yangafc@bc.edu>
2020-07-24 18:36:34 -0400

    Implemented Remote Delegates in "Setup App Remote" right before Initialize App Remote - was able to build and run

--- Bonfire/SceneDelegate.swift
+++ Bonfire/SceneDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate, SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate {
     
+    
     var window: UIWindow?
     
     func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
